d5ae0941afd83305dc1087986a8fa7d5410392e4
SiploxT <102182731+SiploxT@users.noreply.github.com>
2022-07-31 02:12:28 +0200

    Add files via upload

--- SiploxY.js
+++ SiploxY.js
@@ -80,6 +80,13 @@ client.on("message", (message) => {
             message.channel.send(embed)
         })
     }
+    if(message.content.startsWith(prefix + "avatar")) {
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+        let avatar = user.user.displayAvatarURL({ dynamic: true, size: 2048}) 
+
+        message.channel.send(avatar)
+    }
+
 
 });
 client.login(config.token);
