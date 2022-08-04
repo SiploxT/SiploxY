d8f9113bb311708e74f813eac2ce3a9512bc4623
Timmy <nachodortta@hotmail.com>
2022-08-03 21:22:27 -0300

    Fix s!roles

--- Test.js
+++ Test.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json"); 
+const config = require("./config.json");
 
 
 let prefix = config.prefix;
@@ -13,7 +13,7 @@ client.on("message", (message) => {
     if (message.author.bot) return;
 	const args = message.content.trim().split(/ +/g);
     if(message.content.startsWith(prefix + 'help')) {
-        
+
         const embed = new Discord.MessageEmbed()
             .setAuthor(message.author.username, message.author.avatarURL())
             .addField('Ping', 'Comprobará la latencia de la API de Discord')
@@ -27,7 +27,7 @@ client.on("message", (message) => {
             .addField('Pat', 'Acariciarás a la persona que menciones' )
             .addField('Hug', 'Abrazás a  la  persona que menciones')
             .setColor("PURPLE")
-        
+
         message.author.send(embed);
     }
     if(message.content.startsWith(prefix + "ping")) {
@@ -37,14 +37,14 @@ client.on("message", (message) => {
     if(message.content.startsWith(prefix + 'SiploxY')) {
         message.channel.send(`Yo`);
     }
-    if(message.content.startsWith(prefix + 'Nya')) { 
+    if(message.content.startsWith(prefix + 'Nya')) {
         message.channel.send({embed: {
         color: 6816932,
         description: "·w·"
         }})
     }
     if(message.content.startsWith(prefix + "sleep")) {
-        const embedDatos = new Discord.MessageEmbed() 
+        const embedDatos = new Discord.MessageEmbed()
         .setTitle("Zzz")
         .setColor("PURPLE")
         .setDescription("")
@@ -56,21 +56,21 @@ client.on("message", (message) => {
         let respuesta = ["Sis", "Non", "Puede ser", "Es lo mas probable", "Las probabilidades son bajas", "No lo creo", "Definitivamente.", "Definitivamente no."  ]
         var random = respuesta[Math.floor(Math.random() * respuesta.length)]
       const embed = new Discord.MessageEmbed()/
-      
-      
+
+
       message.channel.send(random)
     }
     if(message.content.startsWith(prefix + "dado")) {
         let respuestadado = ["🎲 ¡Te ha salido un **1**!", "🎲 ¡Te ha salido un **2**!", "🎲 ¡Te ha salido un **3**!", "🎲 ¡Te ha salido un **4**!", "🎲 ¡Te ha salido un **5**!", "🎲 ¡Te ha salido un **6**!"]
         var randomdado = respuestadado[Math.floor(Math.random() * respuestadado.length)]
       const embed = new Discord.MessageEmbed()/
-      
-      
+
+
       message.channel.send(randomdado)
     }
     if(message.content.startsWith(prefix + "say")) {
     const args = message.content.slice(5)
-    if(!args) return message.channel.send("Necesitas poner algo para que pueda decirlo ·w·") 
+    if(!args) return message.channel.send("Necesitas poner algo para que pueda decirlo ·w·")
 
     message.channel.send(args)
 
@@ -81,8 +81,8 @@ client.on("message", (message) => {
     if(message.content.startsWith(prefix + "SadCat")) {
      const fetch = require('node-fetch')
      const { MessageEmbed } = require("discord.js")
-    
-    
+
+
      fetch(`https://api.alexflipnote.dev/sadcat`)
         .then((res) => res.json())
         .then((body) => {
@@ -97,8 +97,8 @@ client.on("message", (message) => {
     if(message.content.startsWith(prefix + "dog")) {
      const fetch = require('node-fetch')
      const { MessageEmbed } = require("discord.js")
-    
-    
+
+
      fetch(`https://api.alexflipnote.dev/dogs`)
         .then((res) => res.json())
         .then((body) => {
@@ -113,8 +113,8 @@ client.on("message", (message) => {
     if(message.content.startsWith(prefix + "cat")) {
      const fetch = require('node-fetch')
      const { MessageEmbed } = require("discord.js")
-    
-    
+
+
      fetch(`https://api.alexflipnote.dev/cats`)
         .then((res) => res.json())
         .then((body) => {
@@ -125,17 +125,17 @@ client.on("message", (message) => {
             .setColor("PURPLE")
             message.channel.send(embed)
         })
-   
+
     }
     if(message.content.startsWith(prefix + "avatar")) {
         let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
-        let avatar = user.user.displayAvatarURL({ dynamic: true, size: 2048}) 
+        let avatar = user.user.displayAvatarURL({ dynamic: true, size: 2048})
 
         message.channel.send(avatar)
     }
     if(message.content.startsWith(prefix + "randomuser")) {
-        const embed = new Discord.MessageEmbed() 
-            .setDescription(message.guild.members.cache.random().displayName) 
+        const embed = new Discord.MessageEmbed()
+            .setDescription(message.guild.members.cache.random().displayName)
             .setColor("RANDOM")
         message.channel.send(embed)
     }
@@ -143,11 +143,11 @@ client.on("message", (message) => {
         let user = message.author.username;
         let ment = message.mentions.users.first();
         if(!user) return message.channel.send("Menciona a alguien para poder acariciarlo ·w·")
-        var respuestapat = ["https://c.tenor.com/Y7B6npa9mXcAAAAC/rikka-head-pat-pat-on-head.gif", "https://c.tenor.com/E6fMkQRZBdIAAAAC/kanna-kamui-pat.gif", "https://c.tenor.com/8DaE6qzF0DwAAAAC/neet-anime.gif", "https://c.tenor.com/i7nXGbPLqTsAAAAC/anime-hug.gif", "https://c.tenor.com/kM1mVaXE8Y8AAAAC/kaede-azusagawa-kaede.gif", "https://c.tenor.com/TRxNL32jtEIAAAAC/anime-pat.gif", 
+        var respuestapat = ["https://c.tenor.com/Y7B6npa9mXcAAAAC/rikka-head-pat-pat-on-head.gif", "https://c.tenor.com/E6fMkQRZBdIAAAAC/kanna-kamui-pat.gif", "https://c.tenor.com/8DaE6qzF0DwAAAAC/neet-anime.gif", "https://c.tenor.com/i7nXGbPLqTsAAAAC/anime-hug.gif", "https://c.tenor.com/kM1mVaXE8Y8AAAAC/kaede-azusagawa-kaede.gif", "https://c.tenor.com/TRxNL32jtEIAAAAC/anime-pat.gif",
     "https://c.tenor.com/8o4fWGwBY1EAAAAd/aharensan-aharen.gif", "https://c.tenor.com/jEfC8cztigIAAAAC/anime-pat.gif", "https://c.tenor.com/lOawy4d-SHMAAAAd/anime-cuddle-gauge.gif", "https://c.tenor.com/VzJtkXVo06wAAAAC/yuru-yuri-anime.gif", "https://c.tenor.com/Jj-vHGZOgT4AAAAC/anime-anime-girl.gif", "https://c.tenor.com/jBuHEbqxarcAAAAC/k-on-anime.gif"]
         let randompat = respuestapat[Math.floor(respuestapat.length * Math.random())]
 
-        const embedDatos = new Discord.MessageEmbed() 
+        const embedDatos = new Discord.MessageEmbed()
         .setTitle("")
         .setDescription('**' + user + '**' + ' acarició a **<@' + ment + ">**")
         .setColor("PURPLE")
@@ -163,7 +163,7 @@ client.on("message", (message) => {
     "https://c.tenor.com/vpE5_F_oqmsAAAAC/run-hug-hug.gif", "https://c.tenor.com/-0nQoPY5sZ0AAAAC/anime-hug-hug.gif", "https://c.tenor.com/we1trpFB2F0AAAAC/neko-hug.gif", "https://c.tenor.com/4D5jSREXInMAAAAd/anime-couple-hug.gif", "https://c.tenor.com/QTbBCR3j-vYAAAAd/hugs-best-friends.gif", "https://c.tenor.com/yc_shX2Xl_QAAAAd/girl-anime.gif", "https://c.tenor.com/TJuvig1CFBQAAAAM/the-pet-girl-of-sakurasou-sakurasou-no-pet-na-kanojo.gif" ]
         let randomhug = respuestahug[Math.floor(respuestahug.length * Math.random())]
 
-        const embedDatos = new Discord.MessageEmbed() 
+        const embedDatos = new Discord.MessageEmbed()
         .setTitle("")
         .setDescription('**' + user + '**' + ' abrazó a **<@' + ment + ">**")
         .setColor("PURPLE")
@@ -172,15 +172,15 @@ client.on("message", (message) => {
         message.channel.send({ embed: embedDatos });
     }
     if(message.content.startsWith(prefix + "roles")) {
-        let id = message.guild.id; 
-         const embedRoles = new Discord.RichEmbed() 
+        let id = message.guild;
+         const embedRoles = new Discord.MessageEmbed()
          .setColor("PURPLE")
-         .setDescription(`${client.guilds.get(id).roles.map(r => r.name).join(", ")}`)
+         .setDescription(`${id.roles.cache.map(r => r.name).join(", ")}`)
          .setFooter('Lista de roles de '+ message.guild.name);
-    
-        message.channel.send(embedRoles); 
+
+        message.channel.send(embedRoles);
     }
 
-    
-});  
+
+});
 client.login(config.token);
