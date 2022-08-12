a80906be428b81f41e4f205cddcc09e31eb03ec4
SiploxT <102182731+SiploxT@users.noreply.github.com>
2022-08-13 00:21:05 +0200

    Arreglar comando kill
    Literal no enviaba el mensaje

--- SiploxY.js
+++ SiploxY.js
@@ -191,7 +191,7 @@ client.on("message", (message) => {
         let ment = message.mentions.users.first();
         if(!user) return message.channel.send("Menciona a la persona que quieras matar ·w·")
         var respuestakill = ["https://c.tenor.com/NbBCakbfZnkAAAAC/die-kill.gif", "https://c.tenor.com/Ds187JeCgckAAAAC/animehit-fugirl.gif", "https://c.tenor.com/Ze50E1rW44UAAAAd/akudama-drive.gif", "https://c.tenor.com/t-0fYVPgg1YAAAAC/pink-hair-anime.gif", "https://c.tenor.com/AGTqt-wXyiEAAAAC/nichijou-minigun.gif", "https://c.tenor.com/Mn4W4D899WEAAAAC/ira-gamagoori-attack.gif", "https://c.tenor.com/bznBkYdhexcAAAAC/fire-arm-fire.gif", 
-    "https://c.tenor.com/hkeM4Uie0bcAAAAd/anime-lick-anime-yandere.gif", "https://c.tenor.com/WxLl5mre8pYAAAAd/anime-kill.gif", ""]
+    "https://c.tenor.com/hkeM4Uie0bcAAAAd/anime-lick-anime-yandere.gif", "https://c.tenor.com/WxLl5mre8pYAAAAd/anime-kill.gif", "https://c.tenor.com/nTEMMozvRwIAAAAd/basil-basil-dies.gif", "https://c.tenor.com/wikodIpaz8oAAAAC/omori-basil.gif", "https://c.tenor.com/G9tCUL5OBcYAAAAC/stab-knife.gif", "https://c.tenor.com/FkxPkj7NOrQAAAAd/akame-akame-of-demon-sword-murasame.gif", "https://c.tenor.com/PFndSfQcmRUAAAAd/anime-kill.gif", "https://c.tenor.com/piK8t2UxKZMAAAAC/edward-elric-punch.gif", "https://c.tenor.com/ECYDNFQJHGgAAAAd/hk416-threat.gif", "https://c.tenor.com/6525cG5E7oQAAAAd/anime-kill-kill.gif"]
         let randomkill = respuestakill[Math.floor(respuestakill.length * Math.random())]
 
         const embedDatos = new Discord.MessageEmbed()
@@ -199,6 +199,8 @@ client.on("message", (message) => {
         .setDescription('**' + user + '**' + ' mató a **<@' + ment + ">**")
         .setColor("PURPLE")
         .setImage(randomkill)
+
+        message.channel.send({ embed: embedDatos });
 
     }
     if(message.content.startsWith(prefix + "hug")) {
