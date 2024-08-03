6ff76fe742c6dd1e3275383c7683a389699d25dc
SiploxT <102182731+SiploxT@users.noreply.github.com>
2024-08-03 22:35:21 +0200

    Update SiploxY.py

--- SiploxY.py
+++ SiploxY.py
@@ -28,11 +28,30 @@ async def on_message(message):
             colour=discord.Color.from_rgb(255, 255, 255)
         )
 
-        embed.add_field(name="▸ 🔧 Utilidad", value="> ``servericon (si)`` | ``avatar (a)``")
+        embed.add_field(name="▸ 🔧 Utilidad", value="> ``servericon (si)`` | ``avatar (a)`` | ``banner (b)")
         
         await message.channel.send(embed=embed)
 
     ## Utilidad
+
+    if message.content.startswith(f"{prefix}servericon") or message.content.startswith(f"{prefix}si"):
+        GuildName = message.guild.name
+
+        icon_url_png = message.guild.icon.with_format("png").url
+        icon_url_webp = message.guild.icon.with_format("webp").url
+        icon_url_jpg = message.guild.icon.with_format("jpg").url
+        icon_url_jpeg = message.guild.icon.with_format("jpeg").url
+
+        links_text = f"> [PNG]({icon_url_png}) | [WEBP]({icon_url_webp}) | [JPG]({icon_url_jpg}) | [JPEG]({icon_url_jpeg})"
+
+        embed = discord.Embed(
+            title=f" :art:  | Icono de {GuildName} |",
+            colour=discord.Color.from_rgb(255, 255, 255)
+        )
+        embed.add_field(name="Enlaces a otros formatos", value=links_text, inline=False)
+        embed.set_image(url=icon_url_png)
+
+        await message.channel.send(embed=embed)
 
     if message.content.startswith(f"{prefix}avatar") or message.content.startswith(f"{prefix}a"):
 
@@ -57,24 +76,33 @@ async def on_message(message):
 
         await message.channel.send(embed=embed)
 
-    if message.content.startswith(f"{prefix}servericon") or message.content.startswith(f"{prefix}si"):
-        GuildName = message.guild.name
+    if message.content.startswith(f"{prefix}banner") or message.content.startswith(f"{prefix}b"):
 
-        icon_url_png = message.guild.icon.with_format("png").url
-        icon_url_webp = message.guild.icon.with_format("webp").url
-        icon_url_jpg = message.guild.icon.with_format("jpg").url
-        icon_url_jpeg = message.guild.icon.with_format("jpeg").url
+        if message.mentions:
+            user_id = message.mentions[0].id
+        else:
+            user_id = message.author.id
 
-        links_text = f"> [PNG]({icon_url_png}) | [WEBP]({icon_url_webp}) | [JPG]({icon_url_jpg}) | [JPEG]({icon_url_jpeg})"
+        user = await client.fetch_user(user_id)    
 
-        embed = discord.Embed(
-            title=f" :art:  | Icono de {GuildName} |",
-            colour=discord.Color.from_rgb(255, 255, 255)
-        )
-        embed.add_field(name="Enlaces a otros formatos", value=links_text, inline=False)
-        embed.set_image(url=icon_url_png)
+        if user.banner:
+            banner_url_png = user.banner.with_format("png").url
+            banner_url_webp = user.banner.with_format("webp").url
+            banner_url_jpg = user.banner.with_format("jpg").url
+            banner_url_jpeg = user.banner.with_format("jpeg").url
 
-        await message.channel.send(embed=embed)
+            links_text = f"> [PNG]({banner_url_png}) | [WEBP]({banner_url_webp}) | [JPG]({banner_url_jpg}) | [JPEG]({banner_url_jpeg})"
+
+            embed = discord.Embed(
+                title=f" :art:  | Banner de {user} |",
+                colour=discord.Color.from_rgb(255, 255, 255)
+            )
+            embed.add_field(name="Enlaces a otros formatos", value=links_text, inline=False)
+            embed.set_image(url=banner_url_png)
+
+            await message.channel.send(embed=embed)
+        else:
+            await message.channel.send(f"{user} no tiene un banner.")
 
 # Reemplazar "TOKEN" por una token de bot. (https://discord.com/developers/applications)
 client.run("TOKEN")
